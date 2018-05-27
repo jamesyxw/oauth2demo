4999bfaf59cc99b5a6a67a38e1f253d66368d2be
jamesyxw <james.wang2016@gmail.com>
2018-05-27 15:47:21 -0700

    Enhanced the server.js with api authentication

--- server.js
+++ server.js
@@ -1,21 +1,53 @@
 const request = require("request");
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const morgan = require('morgan')
+const { OAuth2Client } = require('google-auth-library');
 
+const oauthClientId = "622741480313-8a3o12mt0i2pc6tkldcan5afrhlo6cfs.apps.googleusercontent.com";
+
+const jsonParser = bodyParser.json();
+const client = new OAuth2Client(oauthClientId);
 const app = express();
 
-app.get('/oauth/redirect', (req, res) => {
-    console.log("/oauth/redirect");
-    res.sendFile(path.join(__dirname, "webapp", 'redirect.html'));
+app.use(jsonParser);
+app.use(morgan('combined'));
+
+function validateReq(req, res, next) {
+    if (!req.get('Authorization')) {
+        res.status(400).send("Authorization header is required");
+    }
+    const id_token = req.get('Authorization').substring("Bearer ".length);
+    if (!id_token) {
+        res.status(400).send("Id token is required");
+    }
+
+    client.verifyIdToken({
+        idToken: id_token,
+        audience: oauthClientId
+    }, (err, loginTicket) => {
+        if (err) {
+            res.status(401).send("ID token is invalid");
+        }
+
+        const payload = loginTicket.getPayload();
+        req.userContext = payload;
+        next();
+    });
+}
+
+app.use("/api/v1/*", validateReq);
+
+app.get('/api/v1/secret', (req, res) => {
+    res.status(200).send({
+        email: req.userContext.email
+    });
 });
 
-app.get('/login', (req, res) => {
-    console.log("login");
-    res.sendFile(path.join(__dirname, "webapp", 'index.html'));
-});
+
 
 app.use(express.static('webapp'));
-
 app.listen(8080);
 
 
--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,5 @@
 function onSuccess(googleUser) {
     var profile = googleUser.getBasicProfile();
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    // console.log('id_token: ' + googleUser.getAuthResponse().id_token);
-
     var userInfoElem = document.getElementById("userinfo");
     addLiToElement(userInfoElem, "H2", 'User Info');
     addLiToElement(userInfoElem, "LI", 'ID: ' + profile.getId());
@@ -41,10 +35,11 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+        var userInfoElem = document.getElementById("userinfo");
+        while (userInfoElem.firstChild) {
+            userInfoElem.removeChild(userInfoElem.firstChild);
+        }
     });
-    var userInfoElem = document.getElementById("userinfo");
-    while (userInfoElem.firstChild) {
-        userInfoElem.removeChild(userInfoElem.firstChild);
-    }
+    auth2.disconnect();
 }
 
